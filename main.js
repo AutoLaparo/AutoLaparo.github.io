b36504f92c220719d47243847bb570b3957acd43
hkclrofficial <100256250+hkclrofficial@users.noreply.github.com>
2022-03-16 14:23:17 +0800

    format datatime

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ var db = firebase.firestore();
 
 function get_time(){
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var date = today.getFullYear()+'-'+('00'+today.getMonth()+1).slice(-2)+'-'+('00'+today.getDate()).slice(-2);
+    var time = today.getHours() + ":" + ('00'+today.getMinutes()).slice(-2) + ":" + ('00'+today.getSeconds()).slice(-2);
     return date+' '+time;
   
   }
